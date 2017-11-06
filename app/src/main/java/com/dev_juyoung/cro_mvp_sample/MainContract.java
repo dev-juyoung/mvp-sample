5503f26878e210578f28d23e994e9dcb8d756d5d
dev.juyoung <dev.juyoung@gmail.com>
2017-11-06 13:55:16 +0900

    * View 및 Presenter 분리 및 기능 구현.

--- app/src/main/java/com/dev_juyoung/cro_mvp_sample/MainContract.java
+++ app/src/main/java/com/dev_juyoung/cro_mvp_sample/MainContract.java
@@ -1,15 +1,23 @@
 package com.dev_juyoung.cro_mvp_sample;
 
+import android.content.Context;
+
+import com.dev_juyoung.cro_mvp_sample.data.ImageData;
+
+import java.util.ArrayList;
+
 /**
  * Created by juyounglee on 2017. 11. 6..
  */
 
 public interface MainContract {
     interface View {
-
+        void updateView(ArrayList<Integer> items, boolean isUpdate);
     }
 
     interface Presenter {
         void setView(View view);
+        void setImageData(ImageData imageData);
+        void updateData(Context context, boolean isUpdate);
     }
 }
