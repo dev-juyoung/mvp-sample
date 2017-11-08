033208b16f952a3dc9a46bffab8b796b9f8defcf
dev.juyoung <dev.juyoung@gmail.com>
2017-11-08 11:56:11 +0900

    * View 관련 method 추가.

--- app/src/main/java/com/dev_juyoung/cro_mvp_sample/base/BasePresenter.java
+++ app/src/main/java/com/dev_juyoung/cro_mvp_sample/base/BasePresenter.java
@@ -4,5 +4,7 @@ package com.dev_juyoung.cro_mvp_sample.base;
  * Created by juyounglee on 2017. 11. 8..
  */
 
-public interface BasePresenter {
+public interface BasePresenter<T extends BaseView> {
+    void setView(T view);
+    void removeView();
 }
